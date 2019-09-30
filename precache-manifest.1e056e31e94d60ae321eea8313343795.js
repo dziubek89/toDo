d06bd95e2d8ab2f66d1344feb497d939de80f5c5
dziubas <dziubek151@interia.pl>
2019-09-30 22:53:31 +0200

    Updates

---
+++ precache-manifest.1e056e31e94d60ae321eea8313343795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f509b734d827328fbd86d609a14aed",
+    "url": "/toDo/index.html"
+  },
+  {
+    "revision": "ab5ebacb0086b0b7dbf8",
+    "url": "/toDo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8e6aa3ca7871018f25f6",
+    "url": "/toDo/static/css/main.60cd84ff.chunk.css"
+  },
+  {
+    "revision": "ab5ebacb0086b0b7dbf8",
+    "url": "/toDo/static/js/2.9d21d9da.chunk.js"
+  },
+  {
+    "revision": "8e6aa3ca7871018f25f6",
+    "url": "/toDo/static/js/main.060db6ac.chunk.js"
+  },
+  {
+    "revision": "5841f44f5d2bc81b02fd",
+    "url": "/toDo/static/js/runtime-main.9d3b336f.js"
+  }
+]);
